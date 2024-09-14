1d258ce66e1ac16c41ed5ebb5326d60e4e41e006
Matheus de Alencar <matheusdealencargl@gmail.com>
2024-09-14 17:30:40 -0300

    Fundamentos002 - Menu criado

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
-function carlinhos(){
-    console.log("começou")
-    console.log(4-3)
+const { select } = require('@inquirer/prompts')
+
+async function Menu  ()  {
+
+    while (true){
+        const opcao = await select({
+            message: "Menu >",
+            choices:[
+                {
+                name: "Cadastrar Meta",
+                value: "cadastrar"
+                },
+                {
+                    name: "Deletar meta",
+                    value: "deletar"
+                },
+                {
+                    name:"Sair",
+                    value:"sair"
+                }
+            ]
+        })
+        switch (opcao)
+        {
+            case "cadastrar":
+                console.log("vamos cadastrar")
+                break
+            
+            case "deletar":
+                console.log("Vamos deletar")
+                break
+            case "sair":
+                console.log("Saiu.Acabou tudo.")
+                return
+
+        }
+
+    }
+    
 }
- carlinhos()
+Menu()
